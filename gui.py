28f0beb4d4bccfdcd2609284518fdb7b28ed0c74
Connor Truono <ctru14@gmail.com>
2020-11-23 17:24:43 -0500

    Updated GUI for PWM servo control

--- gui.py
+++ gui.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python
 
 import tkinter as tk
-# import RPi.GPIO as GPIO
-# import picamera
+import picamera
+import time
+import RPi.GPIO as GPIO
+from time import sleep
 
+camera = picamera.PiCamera()
+top = tk.Tk()
+pwmPin1 = 13
+pwmPin2 = 11
+solenoid = 15
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pwmPin1, GPIO.OUT)
+GPIO.setup(pwmPin2, GPIO.OUT)
+GPIO.setup(solenoid, GPIO.OUT)
+pwm1=GPIO.PWM(pwmPin1, 50)
+pwm2=GPIO.PWM(pwmPin2, 50)
+pwm1.start(0)
+pwm2.start(0)
+
+def set_angle(angle, pwmPin, pwm):
+    print("setting angle")
+    duty = angle / 18 + 3
+    GPIO.output(pwmPin, True)
+    pwm.ChangeDutyCycle(duty)
+#     sleep(1)
+    GPIO.output(pwmPin, False)
+    pwm.ChangeDutyCycle(duty)
+    
 def fire():
-	print("Firing")
+    GPIO.output(solenoid, True)
+    sleep(0.5)
+    GPIO.output(solenoid, False)
+    print("Firing")
 
 def start_camera():
     camera.preview_fullscreen=False
@@ -13,39 +41,46 @@ def start_camera():
     camera.resolution=(640,480)
     camera.start_preview()
 
-def Zoom(var):
-    x = float("0."+var)
-    print(x)
-    # camera.zoom = (0.5,0.5,x,x)
+def zoom(var):
+    x = (109 - float(var))/100
+    camera.zoom = (0.5,0.5,x,x)
+
+def horizontal_control(var):
+    set_angle(int(var), pwmPin1, pwm1)
+
+def vertical_control(var):
+    set_angle(int(var), pwmPin2, pwm2)
+    print(int(var))
 
 def exit():
     top.destroy
-    # camera.stop_preview()
-    # camera.close()
+    camera.stop_preview()
+    camera.close()
+    pwm.stop()
+    GPIO.cleanup()
     quit()
 
-top = tk.Tk()
-top.resizable(width=False, height=False)
-top.geometry("500x200")
+def setup_gui():
+    top.resizable(width=False, height=False)
+    top.geometry("500x200")
 
-buttonframe = tk.Frame(top, width=500, height=200)
-buttonframe.grid(row=2, column=4, sticky="nesw")
+    buttonframe = tk.Frame(top, width=500, height=500)
+    buttonframe.grid(row=2, column=5, sticky="nesw")
 
-value = tk.DoubleVar()
+    tk.Button(buttonframe, text="Fire!", command=fire).grid(row=2, column=1)
+    tk.Button(buttonframe, text="Start Camera", command=start_camera).grid(row=2, column=2)
+    tk.Button(buttonframe, text="Exit", command=exit).grid(row=2, column=3)
 
-scale = tk.Scale(buttonframe, orient=tk.HORIZONTAL, variable=value, from_=0, to=180, length=300)
-scale.grid(row=1, column=1, columnspan=3)
+    tk.Scale(buttonframe, from_=0, to=180, orient=tk.HORIZONTAL, label = "Horizontal", command=horizontal_control, length=150).grid(row=1, column=1, columnspan=3)
+    tk.Scale(buttonframe, from_=180, to=0, orient=tk.VERTICAL, label = "Vertical", command=vertical_control, length=150).grid(row=1, column=4, rowspan=2)
+    tk.Scale(buttonframe, from_=99, to=10, orient=tk.VERTICAL, label = "Zoom", command=zoom, length=150).grid(row=1,column=5, rowspan=2)
 
-tk.Button(buttonframe, text="Fire!", command=fire).grid(row=2, column=1)
-tk.Button(buttonframe, text="Start Camera", command=start_camera).grid(row=2, column=2)
-tk.Button(buttonframe, text="Exit", command=exit).grid(row=2, column=3)
+    buttonframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    
+    top.mainloop()
 
-tk.Scale(buttonframe, from_=99, to=10, orient=tk.VERTICAL, label = "Zoom", command=Zoom).grid(row=1,column=4, rowspan=2)
+def main():
+    setup_gui()
 
-# button.pack(anchor=tk.CENTER)
-
-# buttonframe.pack(fill="both", expand=True, padx=20, pady=20)
-buttonframe.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-
-top.mainloop()
+if __name__ == "__main__":
+    main()
